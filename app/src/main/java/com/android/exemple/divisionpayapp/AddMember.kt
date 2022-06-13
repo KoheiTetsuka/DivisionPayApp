95d940229b3e272101913a0fe873ebfeb0894c05
tetsukak <58130056+KoheiTetsuka@users.noreply.github.com>
2022-06-13 22:19:56 +0900

    ShoppingDetailsの追加

--- app/src/main/java/com/android/exemple/divisionpayapp/AddMember.kt
+++ app/src/main/java/com/android/exemple/divisionpayapp/AddMember.kt
@@ -1,8 +1,12 @@
 package com.android.exemple.divisionpayapp
 
+import android.content.DialogInterface
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import android.view.View
+import android.widget.*
+import androidx.appcompat.app.AlertDialog
 
 class AddMember : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,8 +15,50 @@ class AddMember : AppCompatActivity() {
 
         val event = intent.getStringExtra("EVENTMAIN")
 
-        val textView = findViewById<TextView>(R.id.textView2)
+        val textView = findViewById<TextView>(R.id.addMemberSum)
         textView.text = event
 
+        val memberAddListView = findViewById<ListView>(R.id.memberAddListView)
+        val memberAddFab = findViewById<View>(R.id.memberAddFab)
+
+        // 1) アダプターにセット
+        val adapter = ArrayAdapter<String>(
+            this,
+            android.R.layout.simple_list_item_1,
+            mutableListOf()
+        )
+
+        memberAddListView.adapter = adapter
+        memberAddListView.onItemClickListener = ListItemClick()
+
+        // main_add_fabが押されたとき
+        memberAddFab.setOnClickListener {
+            // 3) EditText
+            val mainEditText = EditText(this)
+            AlertDialog.Builder(this)
+                .setTitle("項目の追加")
+                .setMessage("何をする？")
+                .setView(mainEditText)
+                .setPositiveButton("追加", DialogInterface.OnClickListener { _, _ ->
+                    val mainPlan = mainEditText.text.toString()
+                    adapter.add(mainPlan)
+                })
+                .setNegativeButton("キャンセル") { dialog, _ ->
+                    // キャンセルボタンを押したときの処理
+                    dialog.dismiss()
+                }
+                .create()
+                .show()
+        }
+    }
+
+    private inner class ListItemClick : AdapterView.OnItemClickListener {
+        override fun onItemClick(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+
+            val member = parent.getItemAtPosition(position) as String
+            val intent = Intent(this@AddMember, ShoppingDetails::class.java)
+            intent.putExtra("MEMBER", member)
+            startActivity(intent)
+        }
     }
 }
